98d4443d55040297a6abd4254f28dcafa1e39d6c
terroroir <81487235+terroroir@users.noreply.github.com>
2025-08-14 16:25:19 -0500

    dd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import App from "./App";
 import { inject } from '@vercel/analytics';
@@ -12,8 +11,6 @@ inject();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App with BrowserRouter */}
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
