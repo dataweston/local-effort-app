4c50160e97022bfa5af921b6483f72bb53a06474
terroroir <81487235+terroroir@users.noreply.github.com>
2025-08-14 23:47:44 -0500

    s

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,20 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // This line is the most important part
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all your React components
   ],
   theme: {
-    extend: {},
+    extend: {
+      // You can extend the default theme here if needed
+      fontFamily: {
+        display: ['General Sans', 'sans-serif'],
+        body: ['Source Sans 3', 'sans-serif'],
+      },
+      colors: {
+        accent: '#f35c2b',
+      }
+    },
   },
   plugins: [],
 }
