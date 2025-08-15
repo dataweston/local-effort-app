85de6cf6951b74260b351f5493263ce8755a8b8c
terroroir <81487235+terroroir@users.noreply.github.com>
2025-08-15 15:26:34 -0500

    s

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -4,6 +4,6 @@ import {createClient} from '@sanity/client'
 export default createClient({
   projectId: 'd6l9d0ea', // Find this in your sanity.json or at manage.sanity.io
   dataset: 'localeffort', // Or the name of your dataset
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
   apiVersion: '2023-05-03', // Use a consistent API version
 })
