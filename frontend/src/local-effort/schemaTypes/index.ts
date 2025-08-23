6e10dc4ab6db34d26cc99c70d97bbe4e6d7b1971
terroroir <81487235+terroroir@users.noreply.github.com>
2025-08-23 13:48:33 -0500

    ddd

--- frontend/src/local-effort/schemaTypes/index.ts
+++ frontend/src/local-effort/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 // Import your new schema
 import menuItems from './menuItems'
+import happyMondayPage from './happyMondayPage'
 
 // Export the schemas as a named constant called 'schemaTypes'
-export const schemaTypes = [menuItems]
+export const schemaTypes = [menuItems, happyMondayPage]
