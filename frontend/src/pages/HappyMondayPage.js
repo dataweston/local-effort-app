fb7554ce5281369ff64a150f87409a2638d00d30
terroroir <81487235+terroroir@users.noreply.github.com>
2025-08-25 15:33:15 -0500

    v

--- frontend/src/pages/HappyMondayPage.js
+++ frontend/src/pages/HappyMondayPage.js
@@ -1,4 +1,4 @@
-import React, 'useState', { useEffect } from 'react';
+import React, {useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { motion, AnimatePresence } from 'framer-motion';
 import BlockContent from '@sanity/block-content-to-react'; // Import the block renderer
