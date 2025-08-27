d348274bbfede4d943ecadc045471d87d9c118f4
terroroir <81487235+terroroir@users.noreply.github.com>
2025-08-27 16:50:44 -0500

    b

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Add this rule
-      include: "**/*.js",
-    }),
-  ],
+  plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
 });
