9169d96df351de9f2d9b1de6c656a15c4313a497
terroroir <81487235+terroroir@users.noreply.github.com>
2025-10-21 21:34:35 -0500

    s

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -9,5 +9,8 @@ export default defineCliConfig({
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  deployment: {autoUpdates: true}
+  deployment: {
+    autoUpdates: true,
+    appId: 'q4scncd6uaeyzxo567jir45u'
+  }
 })
